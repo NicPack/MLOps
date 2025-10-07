3a9b2cf0463567d29d5d0fa2a185746a205d502b
Nicolas Stupak <nicostupak@gmail.com>
2025-10-07 14:37:08 +0200

    feat:prediction

--- lab1/app.py
+++ lab1/app.py
@@ -1,6 +1,15 @@
+import pandas as pd
 from fastapi import FastAPI
 
+from lab1.api.inference import load_model
+from lab1.api.models import PredictRequest, PredictResponse
+from lab1.api.training import load_data
+
 app = FastAPI()
+_, _, class_mapping = load_data()
+
+
+model = load_model("iris_model")
 
 
 @app.get("/")
@@ -11,3 +20,10 @@ def welcome_root():
 @app.get("/health")
 def health_check():
     return {"status": "ok"}
+
+
+@app.post("/predict", response_model=PredictResponse)
+def predict(request: PredictRequest):
+    data = pd.DataFrame(data=request.model_dump(), index=[0])
+    prediction = model.predict(data)[0]
+    return PredictResponse(prediction=class_mapping[prediction])
--- lab1/tests/test_fastapi.py
+++ lab1/tests/test_fastapi.py
@@ -1,4 +1,5 @@
-from app import health_check, welcome_root
+from api.models import PredictRequest
+from app import health_check, predict, welcome_root
 
 
 def test_welcome_root():
@@ -7,3 +8,11 @@ def test_welcome_root():
 
 def test_health_path():
     assert health_check() == {"status": "ok"}
+
+
+def test_prediction():
+    request = PredictRequest(
+        sepal_length=5.1, sepal_width=3.5, petal_length=1.4, petal_width=0.2
+    )
+    response = predict(request)
+    assert response.prediction == "setosa"
