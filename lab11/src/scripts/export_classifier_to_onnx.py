9c4409678803e7e63eab084c7b132c34ede9826d
Nicolas Stupak <nicostupak@gmail.com>
2025-12-22 20:48:01 +0100

    fix: make sure onnx classfier dir exists before saving

--- lab11/src/scripts/export_classifier_to_onnx.py
+++ lab11/src/scripts/export_classifier_to_onnx.py
@@ -3,6 +3,8 @@ import onnx
 from settings import Settings, settings
 from skl2onnx import convert_sklearn
 from skl2onnx.common.data_types import FloatTensorType
+import os
+from pathlib import Path
 
 
 def export_classifier_to_onnx(settings: Settings = settings):
@@ -15,6 +17,11 @@ def export_classifier_to_onnx(settings: Settings = settings):
     print("Converting to ONNX...")
     onnx_model = convert_sklearn(classifier, initial_types=initial_type)
     print(f"Saving ONNX model to {settings.onnx_classifier_path}...")
+
+    onnx_classifier_dir = Path(settings.onnx_classifier_path).parent
+    if not onnx_classifier_dir.exists():
+        onnx_classifier_dir.mkdir(parents=True, exist_ok=True)
+
 
     onnx.save_model(onnx_model, settings.onnx_classifier_path)
 
